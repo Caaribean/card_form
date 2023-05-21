69352642d3ecb8b7e618e2ede71bfe8184e5b873
Caaribean <klaudia.gnatkiewicz@gmail.com>
2023-05-21 11:25:08 +0200

    Add variables to script and add error messages when the fields are incorretly or not filled

---
+++ script.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", function () {
+
+// elements
+const inputCardholderName = document.getElementById('input-cardholder-name');
+const inputCardNumber = document.getElementById('input-card-number');
+const inputMonth = document.getElementById('input-month');
+const inputYear = document.getElementById('input-year');
+const inputCVC = document.getElementById('input-cvc');
+
+const buttonConfirm = document.getElementById('btn-confirm');
+const buttonContinue = document.getElementById('btn-continue');
+
+const cardNumber = document.getElementById('card-number');
+const cardName = document.getElementById('card-name');
+const cardDate = document.getElementById('card-date');
+const cardCVC = document.getElementById('card-cvc');
+
+// error messages
+const errorBlankName = document.getElementById('error-blank-name');
+const errorBlankNumber = document.getElementById('error-blank-number');
+const errorWrongNumber = document.getElementById('error-wrong-number');
+const errorBlankDate = document.getElementById('error-blank-date');
+const errorBlankCVC = document.getElementById('error-blank-cvc');
+const errorWrongCVC = document.getElementById('error-wrong-cvc');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
